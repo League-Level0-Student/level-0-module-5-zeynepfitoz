d6951b5d7635d2d3c26d14a24cdb33a8496e947c
zeynepfitoz <league@wts20.attlocal.net>
2018-06-04 19:55:03 -0700

    Code4life and Circle ring

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,26 +9,46 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
+    	Robot r2d2= new Robot();
 
 
        //  2. Set your robot’s position to x=150 and y=200
+    	r2d2.setX(150);
+    	r2d2.setY(200);
 
         // 3. Put the robot's pen down
+    	r2d2.penDown();
 
         // 4. Set the robot’s speed to 10
+    	r2d2.setSpeed(10);
 
         // 5. Do everything below here 360 times (use i as the counter)
+    	for (int i = 0; i < 360; i++) {
+	
 
 	        // 6. Move the robot 3 pixels
+    		r2d2.move(3);
 
        //  7. Turn the robot 1 degree
+    		r2d2.turn(1);
 
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+    		if (i%20==0) {
+    	
+    		
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
+    			for (int j = 0; j < 360; j++) {
+					
+				
 
 				// 10. Move the robot 1 pixel
+    			r2d2.move(1);
 
 				// 11. Turn the robot 1 degree
+    			r2d2.turn(1);
+    		}
+    		}
+    	}
 }
 }
--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,18 +7,28 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class Code4Life {
+	public static void main(String[] args) {
+		
 	
 	//1.  Ask the user how many hours they spent coding this week.
+	String answer=JOptionPane.showInputDialog("How many hours have you spent coding this week");
 	
 	//2.  If it is less than or equal to 2, tell them to stop watching YouTube and write code instead.
-	
+	if (answer<2){
+		JOptionPane,showMessageDialog(null,"Stop watching YouTube and start coding you idiot!");
+	}
 	//3.  If it is greater than or equal to 3 AND less than or equal to 5, tell them they're a Code Ninja
-	
+	if(answer>3) {
+		JOptionPane.showMessageDialog(null, "You are a code ninja");
+	}
 	//4.  If it is more than 5, call the method below to play the Batman theme song.  
-
+	else {
+		playBatmanTheme();
+	}
     private static void playBatmanTheme() {
     	try {
    		 Clip clip = AudioSystem.getClip();
@@ -29,6 +39,6 @@ public class Code4Life {
      	ex.printStackTrace();
    	}
     }
-
+	}
 
 }

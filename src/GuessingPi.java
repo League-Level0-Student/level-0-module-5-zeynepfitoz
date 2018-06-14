f10df74fd98aebf1d591c3c216a7ed6ddaeefb9b
league <league@wts22.attlocal.net>
2018-06-13 19:56:57 -0700

    part of guessing pi

--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -19,6 +19,7 @@ public class GuessingPi {
 		System.out.println(pi.charAt(0));
 		System.out.println(pi.charAt(1));
 		System.out.println(pi.charAt(2));
+		System.out.println(pi.charAt(3));
 		// 9. If you want to give the user more than one chance to guess,
 		// put a for loop around steps 4-8.
 
@@ -32,6 +33,9 @@ public class GuessingPi {
 
 			// 6. Compare the user's input to the next digit of your pi variable
 			// (look at step 3 for a clue).
+			if (Pi.equals(pi.substring(i, i + 1))) {
+				System.out.println("CORRECT YAY!");
+			}
 
 			// HINT: use charAt(0) to get 1st char of user input String
 

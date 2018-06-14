7067abaa6d7fea8e87e74da303e4c27bab48abac
league <league@wts22.attlocal.net>
2018-06-13 19:56:29 -0700

    Goofy names and part of guesssing pi

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -10,27 +11,35 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-		String name=JOptionPane.showInputDialog("What is your name?");
+		String name = JOptionPane.showInputDialog("What is your name?");
 
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
+		// Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
 
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
+		for (int i = 0; i < name.length(); i++) {
+			String character = name.substring(i, i + 1);
+			if (i % 2 == 0) {
+				goofyName += character.toUpperCase();
+			} else {
+				goofyName += character.toLowerCase();
+			}
+		}
+		JOptionPane.showMessageDialog(null, goofyName);
 
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
 
-		
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
+
+		// 6. ADD this String (containing 1 char) to the goofyName String
+
 		// 7. Use pop-up to show user their Goofy name
 
 	}
 }
-
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -7,32 +8,38 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
+		String pi = "3.1415926535";
 		// 2. Make a String variable to hold the value of Pi.
-		//    Get the first few digits from http://www.piday.org/million/.
+		// Get the first few digits from http://www.piday.org/million/.
 
 		// 3. Print out the first 3 digits of Pi to the console.
-		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
-		//    Run your program to see how this works.
-
+		// The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
+		// Run your program to see how this works.
+		System.out.println(pi.charAt(0));
+		System.out.println(pi.charAt(1));
+		System.out.println(pi.charAt(2));
 		// 9. If you want to give the user more than one chance to guess,
-		//    put a for loop around steps 4-8.
+		// put a for loop around steps 4-8.
 
-			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
-			//    NOTE: The number of digits in your string will be
-			//       yourStringVariable.length()
+		// 4. Create a for loop that will step through each digit of pi (steps 5-8)
+		// NOTE: The number of digits in your string will be
+		// yourStringVariable.length()
+		for (int i = 0; i < pi.length(); i++) {
 
-				// 5. Ask the user for the NEXT digit of pi.
+			// 5. Ask the user for the NEXT digit of pi.
+			String Pi = JOptionPane.showInputDialog("What is the NEXT digit of Pi");
 
-				// 6. Compare the user's input to the next digit of your pi variable 
-				//    (look at step 3 for a clue).
-				//    HINT: use charAt(0) to get 1st char of user input String 
+			// 6. Compare the user's input to the next digit of your pi variable
+			// (look at step 3 for a clue).
 
-					// 7. If they are correct, print out "correct".
+			// HINT: use charAt(0) to get 1st char of user input String
 
-					// 8. If they are not, print out "incorrect" and tell them
-					//    to start over. Use 'break;' to break out of the loop.
+			// 7. If they are correct, print out "correct".
 
+			// 8. If they are not, print out "incorrect" and tell them
+			// to start over. Use 'break;' to break out of the loop.
+
+		}
+	}
 }
-
-

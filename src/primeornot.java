e59cc0f361f06488ec3e265390a5fe2b43e8828d
league <league@wts9.attlocal.net>
2018-06-25 20:01:17 -0700

    prime

--- src/primeornot.java
+++ src/primeornot.java
@@ -1,4 +1,18 @@
+import javax.swing.JOptionPane;
 
 public class primeornot {
+public static void main(String[] args) {
+	String num=JOptionPane.showInputDialog("Pick a random number!");
+	int N=Integer.parseInt(num);
+		if(N%2==0) {
+			 JOptionPane.showMessageDialog(null, "NOT PRIME!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Prime!");
+		}
+	
+		
+	
 
+}
 }

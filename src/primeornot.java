a6ad8097958323357eb16ad67244c2741a353b42
zeynepfitoz <zeynepfitoz@192.168.0.23>
2018-06-25 20:59:56 -0700

    primeornot

--- src/primeornot.java
+++ src/primeornot.java
@@ -4,14 +4,22 @@ public class primeornot {
 public static void main(String[] args) {
 	String num=JOptionPane.showInputDialog("Pick a random number!");
 	int N=Integer.parseInt(num);
-		if(N%2==0) {
-			 JOptionPane.showMessageDialog(null, "NOT PRIME!");
-		}
-		else {
-			JOptionPane.showMessageDialog(null, "Prime!");
-		}
+	boolean notPrime = false;
 	
+	for (int i = 2; i < N; i++) {
 		
+		if( (N%i) == 0) {
+			 JOptionPane.showMessageDialog(null, "NOT PRIME!");
+			 notPrime = true;
+			 break;
+		}
+	}
+	
+	if (notPrime==false)
+	{
+		JOptionPane.showMessageDialog(null, "Prime!");
+	}
+	
 	
 
 }
